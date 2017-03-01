a18c06a6cf199bbd9be76598e2149ddf52730d4d
24billy <deadbilly04@gmail.com>
2017-03-01 20:27:30 +0800

    修改載入property方法

--- src/main/java/tw/com/crm/info360/webapp/controller/BasicController.java
+++ src/main/java/tw/com/crm/info360/webapp/controller/BasicController.java
@@ -1,8 +1,8 @@
 package tw.com.crm.info360.webapp.controller;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.springframework.stereotype.Controller;
@@ -52,37 +52,38 @@ public class BasicController {
 	 * @param model
 	 * 
 	 * @return String 透過字串指定轉導頁面
+	 * @throws IOException
 	 */
 	@RequestMapping("/main") // 作用等同<url-pattern>
 	public String main(@RequestParam(value = "userName", required = false) String userName,
-			@RequestParam(value = "password", required = false) String password, Model model) {
+			@RequestParam(value = "password", required = false) String password, Model model) throws IOException {
 		model.addAttribute("userName", userName);
 		model.addAttribute("password", password);
-		
+
 		Properties prop = new Properties();
-		try {
-			//prop.load(new FileInputStream("config.properties"));
-			prop.load(new FileInputStream(System.getProperty("user.dir")+"/webapps/config.properties"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		String propFileName = "config.properties";
+
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+		if (inputStream != null) {
+			prop.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
 		}
-		
+
 		String websocket_hostname = prop.getProperty("websocket.hostname");
 		String websocket_protocol = prop.getProperty("websocket.protocol");
 		String websocket_port = prop.getProperty("websocket.port");
-		
+
 		String Info360_Setting_protocol = prop.getProperty("Info360_Setting.protocol");
 		String Info360_Setting_hostname = prop.getProperty("Info360_Setting.hostname");
 		String Info360_Setting_port = prop.getProperty("Info360_Setting.port");
-		
+
 		model.addAttribute("websocket_hostname", websocket_hostname);
 		model.addAttribute("websocket_protocol", websocket_protocol);
 		model.addAttribute("websocket_port", websocket_port);
-		
+
 		model.addAttribute("Info360_Setting_protocol", Info360_Setting_protocol);
 		model.addAttribute("Info360_Setting_hostname", Info360_Setting_hostname);
 		model.addAttribute("Info360_Setting_port", Info360_Setting_port);
@@ -92,28 +93,28 @@ public class BasicController {
 
 	/*--------- Tab 分頁導頁控制區 ----------*/
 	@RequestMapping(value = "dashboard")
-	public String showDashBoard(Model model) {
-		
+	public String showDashBoard(Model model) throws IOException {
+
 		Properties prop = new Properties();
-		try {
-			//prop.load(new FileInputStream("config.properties"));
-			prop.load(new FileInputStream(System.getProperty("user.dir")+"/webapps/config.properties"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		String propFileName = "config.properties";
+
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+		if (inputStream != null) {
+			prop.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
 		}
-		
+
 		String IMWebSocket_protocol = prop.getProperty("IMWebSocket.protocol");
 		String IMWebSocket_hostname = prop.getProperty("IMWebSocket.hostname");
 		String IMWebSocket_port = prop.getProperty("IMWebSocket.port");
-		
+
 		model.addAttribute("IMWebSocket_protocol", IMWebSocket_protocol);
 		model.addAttribute("IMWebSocket_hostname", IMWebSocket_hostname);
 		model.addAttribute("IMWebSocket_port", IMWebSocket_port);
-		
+
 		return "info360/dashboard";
 	}
 
@@ -123,24 +124,24 @@ public class BasicController {
 	}
 
 	@RequestMapping(value = "setting")
-	public String showSetting(Model model) {
-		
+	public String showSetting(Model model) throws IOException {
+
 		Properties prop = new Properties();
-		try {
-			//prop.load(new FileInputStream("config.properties"));
-			prop.load(new FileInputStream(System.getProperty("user.dir")+"/webapps/config.properties"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		String propFileName = "config.properties";
+
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+		if (inputStream != null) {
+			prop.load(inputStream);
+		} else {
+			throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
 		}
-		
+
 		String IMWebSocket_protocol = prop.getProperty("IMWebSocket.protocol");
 		String IMWebSocket_hostname = prop.getProperty("IMWebSocket.hostname");
 		String IMWebSocket_port = prop.getProperty("IMWebSocket.port");
-		
+
 		String Info360_Setting_protocol = prop.getProperty("Info360_Setting.protocol");
 		String Info360_Setting_hostname = prop.getProperty("Info360_Setting.hostname");
 		String Info360_Setting_port = prop.getProperty("Info360_Setting.port");
@@ -148,11 +149,11 @@ public class BasicController {
 		model.addAttribute("IMWebSocket_protocol", IMWebSocket_protocol);
 		model.addAttribute("IMWebSocket_hostname", IMWebSocket_hostname);
 		model.addAttribute("IMWebSocket_port", IMWebSocket_port);
-		
+
 		model.addAttribute("Info360_Setting_protocol", Info360_Setting_protocol);
 		model.addAttribute("Info360_Setting_hostname", Info360_Setting_hostname);
 		model.addAttribute("Info360_Setting_port", Info360_Setting_port);
-		
+
 		return "info360/setting";
 	}
 
@@ -166,8 +167,7 @@ public class BasicController {
 	public String showChatV1(@RequestParam(value = "id", required = false) String id,
 			@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "interactionId", required = false) String interactionId,
-			Model model) {
+			@RequestParam(value = "interactionId", required = false) String interactionId, Model model) {
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		model.addAttribute("address", address);
@@ -180,8 +180,7 @@ public class BasicController {
 	public String showChatV2(@RequestParam(value = "id", required = false) String id,
 			@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "interactionId", required = false) String interactionId,
-			Model model) {
+			@RequestParam(value = "interactionId", required = false) String interactionId, Model model) {
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		model.addAttribute("address", address);
@@ -189,13 +188,12 @@ public class BasicController {
 
 		return "info360/chat/chat_v2";
 	}
-	
+
 	@RequestMapping(value = "chat3")
 	public String showChatV3(@RequestParam(value = "id", required = false) String id,
 			@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "interactionId", required = false) String interactionId,
-			Model model) {
+			@RequestParam(value = "interactionId", required = false) String interactionId, Model model) {
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		model.addAttribute("address", address);
@@ -203,13 +201,12 @@ public class BasicController {
 
 		return "info360/chat/chat_v3";
 	}
-	
+
 	@RequestMapping(value = "chat4")
 	public String showChatV4(@RequestParam(value = "id", required = false) String id,
 			@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "interactionId", required = false) String interactionId,
-			Model model) {
+			@RequestParam(value = "interactionId", required = false) String interactionId, Model model) {
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		model.addAttribute("address", address);
@@ -217,13 +214,12 @@ public class BasicController {
 
 		return "info360/chat/chat_v4";
 	}
-	
+
 	@RequestMapping(value = "chat5")
 	public String showChatV5(@RequestParam(value = "id", required = false) String id,
 			@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "address", required = false) String address,
-			@RequestParam(value = "interactionId", required = false) String interactionId,
-			Model model) {
+			@RequestParam(value = "interactionId", required = false) String interactionId, Model model) {
 		model.addAttribute("id", id);
 		model.addAttribute("name", name);
 		model.addAttribute("address", address);
